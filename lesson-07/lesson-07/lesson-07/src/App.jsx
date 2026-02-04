f131c0f41a753524fe71ff80eb4639530a6d15e6
Dominic <dsabatier@gmail.com>
2026-02-04 14:25:38 -0700

    lesson-07 midway

---
+++ lesson-07/lesson-07/lesson-07/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import Results from './components/Results'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header tagline="My sick new tagline" />
+      <Results></Results>
+    </>
+  )
+}
+
+export default App
---
+++ lesson-07/lesson-07/lesson-07/src/components/Results.jsx
@@ -0,0 +1,16 @@
+export default function Results() {
+    return (
+        <section class="h-100">
+            <div class="card h-100">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <strong>Results</strong>
+                    <span class="badge text-bg-secondary">4</span>
+                </div>
+
+                <div class="list-group list-group-flush">
+                    List goes here :)
+                </div>
+            </div>
+        </section>
+    );
+}
